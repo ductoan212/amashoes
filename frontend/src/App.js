a614cbfdfeee45252fa5bf8a79287136924ab19c
Duc Toan <ductoan20102000@gmail.com>
2021-04-04 09:18:20 +0700

    Build product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,32 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Amashoes
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign in</a>
-        </div>
-      </header>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Amashoes
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign in</a>
+          </div>
+        </header>
 
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div>
-              <Product product={product} />
-              <span>
-                adnj
-                <i className="fa fa-start"></i>
-              </span>
-            </div>
-          ))}
-        </div>
-      </main>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
 
-      <footer className="row center">All right reserved</footer>
-    </div>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Rating from '../components/Rating';
+import {Link} from 'react-router-dom';
+import data from '../data';
+
+export default function HomeScreen(props) {
+  const product = data.products.find((x) => x._id === props.match.params.id);
+
+  if (!product) {
+    return <div>Product not found!</div>;
+  }
+
+  console.log(product);
+  return (
+    <div>
+      <Link to="/">Back to result</Link>
+      <div className="row top">
+        <div className="col-2">
+          <img className="large" src={product.image}></img>
+        </div>
+        <div className="col-1">
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </li>
+            <li>Price: ${product.price}</li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <div className="row">
+                  <div>Price</div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Status: </div>
+                  <div>
+                    {console.log(product)}
+                    {product.countInStock > 0 ? (
+                      <span className="success">In Stock</span>
+                    ) : (
+                      <span className="error">Unavailable</span>
+                    )}
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className="primary block">Add to Cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
