353c39f5bda9f0dd21e733009ff2509fb74b6902
Duc Toan <ductoan20102000@gmail.com>
2021-04-09 03:02:52 +0700

    Create place order API

---
+++ backend/models/orderModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const orderShema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        name: { type: String, require: true },
+        qty: { type: Number, require: true },
+        image: { type: String, require: true },
+        price: { type: Number, require: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          require: true,
+        },
+      },
+    ],
+    shippingAddress: {
+      fullName: { type: String, require: true },
+      address: { type: String, require: true },
+      city: { type: String, require: true },
+      postalCode: { type: String, require: true },
+      country: { type: String, require: true },
+    },
+    paymentMethod: { type: String, require: true },
+    itemsPrice: { type: Number, require: true },
+    shippingPrice: { type: Number, require: true },
+    taxPrice: { type: Number, require: true },
+    totalPrice: { type: Number, require: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      require: true,
+    },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderShema);
+export default Order;
