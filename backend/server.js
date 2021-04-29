6fd88763fac6f295e517f54fdda8ca77165fe34e
Duc Toan <ductoan20102000@gmail.com>
2021-04-29 13:14:13 +0700

    add module path

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import path from 'path';
 
 dotenv.config();
 
